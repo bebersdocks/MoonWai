c22d6abc9f144044a2548800f17cc29eb60942f1
bebersdocks <bebersdocks@protonmail.com>
2021-09-25 23:32:48 +0300

    Dal: add user language id

--- MoonWai.Dal/DataModels/User.cs
+++ MoonWai.Dal/DataModels/User.cs
@@ -4,13 +4,20 @@ using LinqToDB.Mapping;
 
 namespace MoonWai.Dal.DataModels
 {
+    public enum LanguageId
+    {
+        English,
+        Russian
+    }
+
     public class User
     {
-        [PrimaryKey, Identity] public int       UserId       { get; set; }
-        [Column,      NotNull] public string    Username     { get; set; }
-        [Column,      NotNull] public byte[]    PasswordSalt { get; set; }
-        [Column,      NotNull] public byte[]    PasswordHash { get; set; }
-        [Column,      NotNull] public DateTime  CreateDt     { get; set; }
-        [Column,     Nullable] public DateTime? LastAccessDt { get; set; }
+        [PrimaryKey, Identity] public int        UserId       { get; set; }
+        [Column,      NotNull] public string     Username     { get; set; }
+        [Column,      NotNull] public byte[]     PasswordSalt { get; set; }
+        [Column,      NotNull] public byte[]     PasswordHash { get; set; }
+        [Column,      NotNull] public LanguageId LanguageId   { get; set; }
+        [Column,      NotNull] public DateTime   CreateDt     { get; set; }
+        [Column,     Nullable] public DateTime?  LastAccessDt { get; set; }
     }
 }
