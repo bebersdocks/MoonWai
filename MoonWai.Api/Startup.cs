759d085f5d7c3d5a2788757544f48f24be8e4f67
bebersdocks <bebersdocks@protonmail.com>
2021-09-25 20:20:08 +0300

    Api: startup changes

--- MoonWai.Api/Startup.cs
+++ MoonWai.Api/Startup.cs
@@ -1,39 +1,59 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+using MoonWai.Dal;
 
 namespace MoonWai.Api
 {
     public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+    {   
+        public readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
+
+            var dbSettings = configuration.GetSection("DbSettings").Get<DbSettings>();
+
+            Dc.CreateDefaultConfiguration(dbSettings);
+
+            using var dc = new Dc();
+
+            dc.CreateTables();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAuthentication();
+            services.AddAuthorization();
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error/Exception");
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
