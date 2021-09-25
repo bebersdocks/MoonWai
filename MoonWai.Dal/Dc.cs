70b6d5cf41438dd4db9a154f1a8863b85e383198
bebersdocks <bebersdocks@protonmail.com>
2021-09-25 18:21:16 +0300

    Dal: dc default configuration add

--- MoonWai.Dal/Dc.cs
+++ MoonWai.Dal/Dc.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 using MoonWai.Dal.DataModels;
 
@@ -15,9 +16,43 @@ namespace MoonWai.Dal
         public ITable<Thread>       Threads       { get { return this.GetTable<Thread>(); } }
         public ITable<User>         Users         { get { return this.GetTable<User>(); } }
 
-		public Dc(string configuration)
-			: base(configuration)
-		{
-		}
+        private const string defaultConfigurationStr = "MoonWai";
+
+        public static void CreateDefaultConfiguration(DbSettings dbSettings)
+        {
+            var connStr = dbSettings.GetConnStr();
+
+            var dataProvider = GetDataProvider(ProviderName.PostgreSQL, connStr);
+            
+            if (dataProvider == null) 
+                throw new Exception("Unable to get data provider");
+
+            AddConfiguration(defaultConfigurationStr, connStr, dataProvider);
+        }
+
+        public Dc() : base(defaultConfigurationStr) { }
+
+		public Dc(string configurationStr) : base(configurationStr) { }
+
+        public void CreateTables()
+        {
+            var schemaProvider = DataProvider.GetSchemaProvider();
+            var dbSchema = schemaProvider.GetSchema(this);
+
+            void createTable<T>(ITable<T> table)
+            {
+                if (!dbSchema.Tables.Any(i => i.TableName == table.TableName))
+                {
+                    this.CreateTable<T>();
+                }
+            }
+
+            createTable(Boards);
+            createTable(Media);
+            createTable(Posts);
+            createTable(PostResponses);
+            createTable(Threads);
+            createTable(Users);
+        }
     }
 }
