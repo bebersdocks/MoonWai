6e82b957de34a46d6abfe40c10f9970a3cc78ecd
bebersdocks <bebersdocks@protonmail.com>
2021-10-10 17:55:00 +0300

    Elmish: add http get

--- MoonWai.Elmish/src/Http.fs
+++ MoonWai.Elmish/src/Http.fs
@@ -1,19 +1,9 @@
 module Http
 
-open Fable.Core.JsInterop
 open Fetch
-open Thoth.Json
 
-let inline post<'a, 'b> url (obj: 'a) (ofSuccess: string -> 'b) (ofFailure: string -> 'b) = 
-    promise { 
-        let body = Encode.Auto.toString<'a>(0, obj)
-
-        let props = [ 
-            Method HttpMethod.POST
-            requestHeaders [ ContentType "application/json" ]
-            Body !^body 
-        ]
-
+let inline private request<'b> url props (ofSuccess: string -> 'b) (ofFailure: string -> 'b) = 
+    promise {
         try
             let! resp = fetchUnsafe url props
             let! txt = resp.text()
@@ -27,3 +17,20 @@ let inline post<'a, 'b> url (obj: 'a) (ofSuccess: string -> 'b) (ofFailure: stri
         with 
         | exn -> return ofFailure exn.Message
     }
+
+let inline get<'b> url (ofSuccess: string -> 'b) (ofFailure: string -> 'b) = 
+    request url [ Method HttpMethod.GET ] ofSuccess ofFailure
+
+open Fable.Core.JsInterop
+open Thoth.Json
+
+let inline post<'a, 'b> url (obj: 'a) (ofSuccess: string -> 'b) (ofFailure: string -> 'b) = 
+    let body = Encode.Auto.toString<'a>(0, obj)
+
+    let props = [ 
+        Method HttpMethod.POST
+        requestHeaders [ ContentType "application/json" ]
+        Body !^body 
+    ]
+
+    request url props ofSuccess ofFailure
